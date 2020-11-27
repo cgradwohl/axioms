3a117a17279da923e1be71ee9a1c334778ce49b8
Chris Gradwohl <christophergradwohl@gmail.com>
2020-11-26 20:51:49 -0800

    generics.

--- Languages/Typescript/src/index.ts
+++ Languages/Typescript/src/index.ts
@@ -108,3 +108,26 @@ type Puppy = {
 // thread is a good place to start:
 
 // https://stackoverflow.com/questions/37233735/typescript-interfaces-vs-types/52682220#52682220
+
+// Generics
+// Generics provide variables to types. A common example is an array.
+// An array without generics could contain anything. 
+// An array with generics can describe the values that the array contains.
+type StringArray = Array<string>;
+type NumberArray = Array<number>;
+type ObjectWithNameArray = Array<{ name: string }>;
+
+interface Backpack<Type> {
+  add: (obj: Type) => void;
+  get: () => Type;
+}
+
+// This line is a shortcut to tell TypeScript there is a
+// constant called `backpack`, and to not worry about where it came from.
+declare const backpack: Backpack<string>;
+
+// object is a string, because we declared it above as the variable part of Backpack.
+const object = backpack.get();
+
+// Since the backpack variable is a string, you can't pass a number to the add function.
+backpack.add(23);
